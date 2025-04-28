bc0b3b58e91ff8c7ae773058e65b3e50441a71e1
lecterkn <lecterkn@proton.me>
2025-04-29 00:29:24 +0900

    メッセージの上限と下限を環境変数から設定 #16

--- internal/app/usecase/generate_usecase.go
+++ internal/app/usecase/generate_usecase.go
@@ -1,7 +1,10 @@
 package usecase
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"unicode/utf8"
 
 	"github.com/lecterkn/kaneki_bot/internal/app/port"
@@ -16,18 +19,22 @@ const (
 
 type GenerateUsecase struct {
 	generateRepository port.GenerateRepository
+	messageMin         int
+	messageMax         int
 }
 
 func NewGenerateUsecase(generateRepository port.GenerateRepository) *GenerateUsecase {
 	return &GenerateUsecase{
 		generateRepository,
+		getMessageMin(),
+		getMessageMax(),
 	}
 }
 
 func (u *GenerateUsecase) GenerateReply(cmd input.GenerateCommandInput) (*output.GenerateCommandOutput, error) {
 	// バリデーション
-	if utf8.RuneCountInString(cmd.Message) > MAX_MESSAGE_LEN ||
-		utf8.RuneCountInString(cmd.Message) < MIN_MESSAGE_LEN {
+	if utf8.RuneCountInString(cmd.Message) > u.messageMax ||
+		utf8.RuneCountInString(cmd.Message) < u.messageMin {
 		log.Println("受け取ったメッセージ:", cmd.Message)
 		return &output.GenerateCommandOutput{
 			Content: "黙れ",
@@ -43,4 +50,32 @@ func (u *GenerateUsecase) GenerateReply(cmd input.GenerateCommandInput) (*output
 		Content: *content,
 	}
 	return &cmdOutput, nil
+}
+
+func getMessageMax() int {
+	limit, ok := os.LookupEnv("MAX_MESSAGE_LEN")
+	if !ok {
+		fmt.Println("max not found")
+		return MAX_MESSAGE_LEN
+	}
+	value, err := strconv.Atoi(limit)
+	if err != nil {
+		fmt.Println(err.Error())
+		return MAX_MESSAGE_LEN
+	}
+	return value
+}
+
+func getMessageMin() int {
+	limit, ok := os.LookupEnv("MIN_MESSAGE_LEN")
+	if !ok {
+		fmt.Println("min not found")
+		return MIN_MESSAGE_LEN
+	}
+	value, err := strconv.Atoi(limit)
+	if err != nil {
+		fmt.Println(err.Error())
+		return MIN_MESSAGE_LEN
+	}
+	return value
 }
